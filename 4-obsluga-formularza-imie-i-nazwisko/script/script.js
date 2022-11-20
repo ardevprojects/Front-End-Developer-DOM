768b77358060d0e664c57906acc7229dd8732ed2
rudenkogh <116075187+rudenkogh@users.noreply.github.com>
2022-11-20 09:47:53 +0100

    Changes

---
+++ 4-obsluga-formularza-imie-i-nazwisko/script/script.js
@@ -0,0 +1,14 @@
+console.log('warsztat obsługa formularza');
+
+const submitForm = (event) => {
+    event.preventDefault();
+    
+    let firstName = document.querySelector('[name="fname"]');
+    let lastName = document.querySelector('[name="lname"]');
+
+    console.log(`Imię: ${firstName.value}, Nazwisko: ${lastName.value}`)
+}
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', submitForm);
